34432791335a8a2baad36b29371c5709d5e70b9b
Noah Li <15074002@edu.cbe.ab.ca>
2022-09-12 11:50:31 -0600

    completed search algorithm

--- linear-search.js
+++ linear-search.js
@@ -1,12 +1,9 @@
 function linearSearch(array, searchTerm) {
-	/*
-	 ** Check each element from start to finish
-	 ** until you find an element that matches the search term
-	 **
-	 ** Return the index of the found element
-	 */
-
-	return null;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === searchTerm) {
+			return i
+		}
+	}
 }
 
 module.exports = linearSearch;
