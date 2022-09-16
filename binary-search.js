93fe4f3287ef969863c67053a63b6d345d1a8c7d
Danlert <commenttake@gmail.com>
2022-09-15 20:38:16 -0600

    worked on binary search

--- binary-search.js
+++ binary-search.js
@@ -1,33 +1,33 @@
-function make_aux_array(arr, n)
-{
-let aux_arr = []
-  // For every element in array write
-  // elements and their indices in
-  // auxiliary array of pairs
-  for (let i = 0; i < n; i++) {
-    aux_arr.push([arr[i], i]);
-  }
-  console.table(aux_arr);
-  // sort auxiliary array
-  aux_arr.sort();
-  console.table(aux_arr);
-
-}
-
 function binarySearch(array, searchTerm) {
-	make_aux_array(array, array.length)
 	/*
 	 ** Check the middle element, if it's higher than the searchTerm
 	 ** then check the element in between the start and the middle, otherwise
 	 ** check the element between the middle and the end
-
+	
 	 ** repeat the above process for the subset of the array
 	 ** keep repeating with smaller and smaller subsets until you find the searchTerm
 	 */
-	// array.sort((a,b) => { 
-	// 	a - b 
-	// })
-	// return null;
+	 // search term 5 array[i] = 4
+	///
+	let low = 0
+	let high = array.length
+	while (high - low > 1){
+		
+		let mid = low + (high - low) / 2
+		console.log(low, high, high - low > 1, mid, (high - low)/2);
+
+		if (searchTerm == array[mid]){
+			return array[mid]
+		}
+		if (searchTerm > array[mid]) {
+			low = mid + 1
+		}
+		if (searchTerm < array[mid]){
+			high = mid - 1
+		}
+		
+	}
+	
 }
 
 module.exports = binarySearch;
